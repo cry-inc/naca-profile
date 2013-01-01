e3e8557137d9f4ad765fbc400c5dcf557a9ddb19
cry-inc <cry-inc@users.noreply.github.com>
2013-01-01 22:01:37 +0100

    Workaround for spline generation exception

--- Spline.cs
+++ Spline.cs
@@ -14,8 +14,16 @@ namespace NacaProfile
                 xy[i,1] = points[i].Y;
             }
 
-            alglib.pspline2interpolant s;
-            alglib.pspline2build(xy, points.Length, 2, 2, out s);
+            alglib.pspline2interpolant interpolant;
+
+            try
+            {
+                alglib.pspline2build(xy, points.Length, 2, 2, out interpolant);
+            }
+            catch
+            {
+                return points;
+            }
 
             float stepSize = 1.0f / segments;
             PointF[] path = new PointF[segments + 1];
@@ -23,7 +31,7 @@ namespace NacaProfile
             for (int i = 0; i < segments; i++)
             {
                 double x, y;
-                alglib.pspline2calc(s, pos, out x, out y);
+                alglib.pspline2calc(interpolant, pos, out x, out y);
                 path[i] = new PointF((float)x, (float)y);
                 pos += stepSize;
             }
